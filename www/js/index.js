f6607176272f921c8a4991a3b27dec5fbbaf418a
Atsushi Nakatsugawa <atsushi@moongift.jp>
2016-09-23 14:03:40 +0900

    Update keys

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
-var application_key = "d288714a5a801f4ccaaac99c87df41d35e38b5804a9ecbcd2026c1901e914fc0";
-var client_key = "3395ea58a34af1edb5009c9d15b3379761539ef3c8eb0ee0d797274e122359b8";
+var application_key = "YOUR_APPLICATION_KEY";
+var client_key = "YOUR_CLIENT_KEY";
 
 var ncmb = new NCMB(application_key, client_key);
 
